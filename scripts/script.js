1fd4b96e45152ecd8d001035a0842ef75795e768
Dizth <tomdalessandro80@gmail.com>
2022-01-31 10:13:22 +0100

    safe-build + added minor change on menu (left part)

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,19 @@
 const btnMenu = document.getElementById('menuButton')
 const menuWrapper = document.getElementById('menuWrapper')
 const spanWrapper = document.getElementById('spanWrapper')
+const outsideMenu = document.getElementById('outsideMenu')
+const menuContainer = document.getElementById('menuContainer')
 
 btnMenu.addEventListener('click', (e) =>  {
     menuWrapper.classList.toggle('displayMenu')
     spanWrapper.classList.toggle('displayColor')
+    outsideMenu.classList.toggle('displayOutside')
+    menuContainer.classList.toggle('displayMenuContainer')
+})
+
+outsideMenu.addEventListener('click', (e) => {
+    menuWrapper.classList.remove('displayMenu')
+    spanWrapper.classList.remove('displayColor')
+    outsideMenu.classList.remove('displayOutside')
+    menuContainer.classList.remove('displayMenuContainer')
 })
