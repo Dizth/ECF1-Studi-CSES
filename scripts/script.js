fa8e8f28ab52b92a76c4b1e0149cac3e3b038f1a
Dizth <tomdalessandro80@gmail.com>
2022-02-10 19:22:40 +0100

    made a script for videos on mouseover

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,11 @@ const spanWrapper = document.getElementById('spanWrapper')
 const outsideMenu = document.getElementById('outsideMenu')
 const menuContainer = document.getElementById('menuContainer')
 
+const sideCover = document.querySelector('.side-cover')
+const play = document.querySelector('.placeholder')
+const btnChess = document.getElementById('btnChess')
+const chessVid = document.querySelector('.chess-vid')
+
 btnMenu.addEventListener('click', (e) =>  {
     menuWrapper.classList.toggle('displayMenu')
     spanWrapper.classList.toggle('displayColor')
@@ -21,4 +26,19 @@ outsideMenu.addEventListener('click', (e) => {
     menuWrapper.classList.remove('displayMenu')
     spanWrapper.classList.remove('displayColor')
     outsideMenu.classList.remove('displayOutside')
+})
+
+
+btnChess.addEventListener('mouseover', (e) => {
+    if (sideCover.clientWidth > 0) {
+        chessVid.style.display ='block'
+        chessVid.play()
+        play.style.display ='none'
+    }
+})
+
+btnChess.addEventListener('mouseout',(e) => {
+    chessVid.style.display ='none'
+    play.style.display ='block'
+    chessVid.pause()
 })
