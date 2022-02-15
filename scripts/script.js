929a748c1e5633681856652ed6299fb63724c87d
Dizth <tomdalessandro80@gmail.com>
2022-02-15 16:25:50 +0100

    added modal + form for subscriptions

--- scripts/script.js
+++ scripts/script.js
@@ -32,14 +32,13 @@ btnMenu.addEventListener('click', (e) =>  {
     outsideMenu.classList.toggle('displayOutside')
     if (menuWrapper.classList[1] === 'displayMenu') {
         menuContainer.style.height = '100%'
-        //document.body.style.overflowY = 'hidden'
     } else {
         menuContainer.style.height = '0%'
-        //document.body.style.overflowY = 'visible'
     }
 })
 
 outsideMenu.addEventListener('click', (e) => {
+    menuContainer.style.height = '0%'
     menuWrapper.classList.remove('displayMenu')
     spanWrapper.classList.remove('displayColor')
     outsideMenu.classList.remove('displayOutside')
